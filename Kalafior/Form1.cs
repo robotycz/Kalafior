ae1a8cf1fe55df57f4fda0466b896ac50106b6a9
rybotycz <j.francuz0@gmail.com>
2018-10-23 19:23:43 +0200

    bedzie dobrze

--- Kalafior/Form1.cs
+++ Kalafior/Form1.cs
@@ -16,5 +16,10 @@ namespace Kalafior
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bedzie dobrze
+        }
     }
 }
